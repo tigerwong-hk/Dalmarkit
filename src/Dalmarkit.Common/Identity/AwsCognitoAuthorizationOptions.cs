ee44b497490ebd4e4b3c22719f0345d01b005f38
tigerwong-hk <113076176+tigerwong-hk@users.noreply.github.com>
2024-04-12 17:07:33 +0800

    feat: community user/tenant admin client ids/groups for authorization options

--- src/Dalmarkit.Common/Identity/AwsCognitoAuthorizationOptions.cs
+++ src/Dalmarkit.Common/Identity/AwsCognitoAuthorizationOptions.cs
@@ -5,7 +5,10 @@ public class AwsCognitoAuthorizationOptions
     public string? BackofficeAdminClientIds { get; set; }
     public string? BackofficeAdminGroups { get; set; }
     public string? BackofficeAdminScopes { get; set; }
+    public string? CommunityUserClientIds { get; set; }
+    public string? CommunityUserGroups { get; set; }
     public string? CommunityUserScopes { get; set; }
+    public string? TenantAdminClientIds { get; set; }
     public string? TenantAdminGroups { get; set; }
     public string? TenantAdminScopes { get; set; }
 }
