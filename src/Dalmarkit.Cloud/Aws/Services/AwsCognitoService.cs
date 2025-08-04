c6d0bce25a0924c8c85f0812efcc677f1039fced
tigerwong-hk <113076176+tigerwong-hk@users.noreply.github.com>
2025-08-04 17:16:01 +0800

    fix: set email and phone verified to true

--- src/Dalmarkit.Cloud/Aws/Services/AwsCognitoService.cs
+++ src/Dalmarkit.Cloud/Aws/Services/AwsCognitoService.cs
@@ -48,7 +48,9 @@ public class AwsCognitoService(IAmazonCognitoIdentityProvider cognitoService, IL
             UserAttributes =
                 [
                     new AttributeType { Name = "email", Value = emailAddress },
-                    new AttributeType { Name = "phone_number", Value = phoneNumber }
+                    new AttributeType { Name = "email_verified", Value = "true" },
+                    new AttributeType { Name = "phone_number", Value = phoneNumber },
+                    new AttributeType { Name = "phone_number_verified", Value = "true" },
                 ],
             Username = emailAddress,
             UserPoolId = identityProviderId,
