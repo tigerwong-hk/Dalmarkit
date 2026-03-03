25b151f7ab78d801505b8008d391249a25dafaf8
tigerwong-hk <113076176+tigerwong-hk@users.noreply.github.com>
2026-03-04 05:02:41 +0800

    refactor: signalr pub/sub

--- src/Dalmarkit.AspNetCore/PubSub/SignalR/SignalRTopicHubBase.cs
+++ src/Dalmarkit.AspNetCore/PubSub/SignalR/SignalRTopicHubBase.cs
@@ -1,32 +1,46 @@
 using Dalmarkit.Common.PubSub;
 using Dalmarkit.Common.PubSub.SignalR;
 using Microsoft.AspNetCore.SignalR;
+using System.Collections.Immutable;
 
 namespace Dalmarkit.AspNetCore.PubSub.SignalR;
 
-public class SignalRTopicHubBase(ITopicSubscriptionService subscriptionService) : Hub<ISignalRDataClient>
+public abstract class SignalRTopicHubBase(ITopicSubscriptionService subscriptionService) : Hub<ISignalRDataClient>
 {
     private readonly ITopicSubscriptionService _subscriptionService = subscriptionService ?? throw new ArgumentNullException(nameof(subscriptionService));
 
-    public virtual async Task<IReadOnlyCollection<string>> GetTopicSubscriptionsAsync()
+    public virtual ImmutableHashSet<string> GetTopicSubscriptions()
     {
-        return await _subscriptionService.GetSubscribedTopicsByConnectionIdAsync(Context.ConnectionId);
+        return _subscriptionService.GetSubscriberTopics(Context.ConnectionId);
     }
 
-    public virtual async Task<IReadOnlyCollection<string>> RemoveConnectionAsync(string connectionId)
+    public virtual async Task<ImmutableHashSet<string>> RemoveConnectionAsync(string connectionId)
     {
-        return await _subscriptionService.RemoveConnectionAsync(Context.ConnectionId);
+        ImmutableHashSet<string> topicsRemoved = _subscriptionService.RemoveSubscriber(Context.ConnectionId, GetTopicPrefix);
+        foreach (string topic in topicsRemoved)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, topic);
+        }
+
+        return topicsRemoved;
     }
 
-    public virtual async Task SubscribeTopicAsync(string topic)
+    public virtual async Task<bool> SubscribeTopicAsync(string topic)
     {
-        await _subscriptionService.SubscribeTopicAsync(Context.ConnectionId, topic);
-        await Groups.AddToGroupAsync(Context.ConnectionId, topic);
+        bool isSubscribed = _subscriptionService.SubscribeTopic(Context.ConnectionId, topic, GetTopicPrefix);
+        if (isSubscribed)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, topic);
+        }
+
+        return isSubscribed;
     }
 
-    public virtual async Task UnsubscribeTopicAsync(string topic)
+    public virtual async Task<bool> UnsubscribeTopicAsync(string topic)
     {
         await Groups.RemoveFromGroupAsync(Context.ConnectionId, topic);
-        await _subscriptionService.UnsubscribeTopicAsync(Context.ConnectionId, topic);
+        return _subscriptionService.UnsubscribeTopic(Context.ConnectionId, topic, GetTopicPrefix);
     }
+
+    protected abstract string GetTopicPrefix(string topicName);
 }
