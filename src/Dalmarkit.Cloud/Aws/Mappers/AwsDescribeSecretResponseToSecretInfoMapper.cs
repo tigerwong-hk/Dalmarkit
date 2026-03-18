51c9fea6cd7edce9d3a131405a3789ff67651eb6
tigerwong-hk <113076176+tigerwong-hk@users.noreply.github.com>
2026-03-19 01:40:43 +0800

    chore: remove automapper

--- src/Dalmarkit.Cloud/Aws/Mappers/AwsDescribeSecretResponseToSecretInfoMapper.cs
+++ src/Dalmarkit.Cloud/Aws/Mappers/AwsDescribeSecretResponseToSecretInfoMapper.cs
@@ -7,10 +7,10 @@ namespace Dalmarkit.Cloud.Aws.Mappers;
 [Mapper(RequiredMappingStrategy = RequiredMappingStrategy.Target)]
 public static partial class AwsDescribeSecretResponseToSecretInfoMapper
 {
-    [MapProperty(nameof(SecretListEntry.KmsKeyId), nameof(SecretInfo.EncryptionKeyId))]
-    [MapProperty(nameof(SecretListEntry.Description), nameof(SecretInfo.SecretDescription))]
-    [MapProperty(nameof(SecretListEntry.ARN), nameof(SecretInfo.SecretId))]
-    [MapProperty(nameof(SecretListEntry.Name), nameof(SecretInfo.SecretName))]
+    [MapProperty(nameof(DescribeSecretResponse.KmsKeyId), nameof(SecretInfo.EncryptionKeyId))]
+    [MapProperty(nameof(DescribeSecretResponse.Description), nameof(SecretInfo.SecretDescription))]
+    [MapProperty(nameof(DescribeSecretResponse.ARN), nameof(SecretInfo.SecretId))]
+    [MapProperty(nameof(DescribeSecretResponse.Name), nameof(SecretInfo.SecretName))]
     public static partial SecretInfo ToTarget(DescribeSecretResponse source);
 
     public static partial IEnumerable<SecretInfo> ToTarget(IEnumerable<DescribeSecretResponse> source);
