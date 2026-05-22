5e8b912d8f8f4f0d53f39c9d0c9e6c811ec9ef9f
tigerwong-hk <113076176+tigerwong-hk@users.noreply.github.com>
2026-05-22 11:42:48 +0800

    feat: websocket authentication state, restart authentication when token refresh fails

--- src/Dalmarkit.Common/Services/WebSocketServices/IClientWebSocketService.cs
+++ src/Dalmarkit.Common/Services/WebSocketServices/IClientWebSocketService.cs
@@ -6,12 +6,22 @@ namespace Dalmarkit.Common.Services.WebSocketServices;
 public interface IClientWebSocketService : IDisposable
 {
     string? AuthenticationId { get; }
+    bool IsAuthenticated { get; }
 
     Task ConnectAsync(string? authenticationId, CancellationToken cancellationToken = default);
     Task DisconnectAsync(CancellationToken cancellationToken = default);
+    WebSocketAuthenticationState GetAuthenticationState();
     WebSocketConnectionState GetConnectionState();
     List<string> GetSubscribedChannels();
     Task SendNotificationAsync<TMessage>(TMessage message, CancellationToken cancellationToken = default);
     Task<TResponse?> SendRequestAsync<TParams, TResponse>(JsonRpc2RequestDto<TParams> request, CancellationToken cancellationToken = default)
         where TResponse : class;
+}
+
+public enum WebSocketAuthenticationState
+{
+    Unauthenticated = 1000,
+    AuthenticationSuccessful = 2000,
+    AuthenticationFailed = 3000,
+    Disconnected = 4000
 }
