bf8e9e6ba1162b62626b165d5cff8f78963ea351
Tiger Wong <113076176+tigerwong-hk@users.noreply.github.com>
2026-08-12 12:57:22 +0800

    fix(converters): make BigIntegerJsonConverter able to read its own output and BigIntegerJsonConverter/DecimalJsonConverter raise JsonException

--- src/Dalmarkit.Common/Converters/BigIntegerJsonConverter.cs
+++ src/Dalmarkit.Common/Converters/BigIntegerJsonConverter.cs
@@ -1,5 +1,7 @@
+using System.Buffers;
 using System.Globalization;
 using System.Numerics;
+using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
@@ -9,19 +11,33 @@ public class BigIntegerJsonConverter : JsonConverter<BigInteger>
 {
     public override BigInteger Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
-        string? jsonToken = reader.GetString();
-#pragma warning disable IDE0046 // Convert to conditional expression
+        string? jsonToken;
+        if (reader.TokenType == JsonTokenType.Number)
+        {
+            ReadOnlySpan<byte> utf8 = reader.HasValueSequence ? reader.ValueSequence.ToArray() : reader.ValueSpan;
+            jsonToken = Encoding.UTF8.GetString(utf8);
+        }
+        else
+        {
+            jsonToken = reader.TokenType == JsonTokenType.String
+                ? reader.GetString()
+                : throw new JsonException($"Unable to handle {reader.TokenType}");
+        }
+
         if (string.IsNullOrWhiteSpace(jsonToken))
         {
             throw new JsonException("Unable to get JSON token");
         }
-#pragma warning restore IDE0046 // Convert to conditional expression
 
-        return BigInteger.Parse(jsonToken, NumberStyles.Integer, NumberFormatInfo.InvariantInfo);
+        // TryParse, not Parse: a FormatException from a converter escapes model binding as a 500,
+        // while a JsonException is what the input formatter turns into a 400.
+        return BigInteger.TryParse(jsonToken, NumberStyles.Integer, NumberFormatInfo.InvariantInfo, out BigInteger result)
+            ? result
+            : throw new JsonException($"Unable to convert JSON token to {nameof(BigInteger)}");
     }
 
     public override void Write(Utf8JsonWriter writer, BigInteger value, JsonSerializerOptions options)
     {
-        writer.WriteRawValue(value.ToString(NumberFormatInfo.InvariantInfo));
+        writer.WriteStringValue(value.ToString(NumberFormatInfo.InvariantInfo));
     }
 }
